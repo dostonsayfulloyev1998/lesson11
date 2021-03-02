089b4861d8738a8349d99f50436f221c1f6a296a
Doston Sayfulloyev <sayfulloyevdoston@gmail.com>
2021-03-02 11:13:59 +0500

    table dasturi

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+     
+     <div class="container">
+         <div class="row">
+             <div class="col-md-6">
+                  <table class=" table ">
+                      
+                      <thead class="table-dark">
+                            <tr>
+                                <th>id </th> 
+                                <th>name</th>
+                                <th>surname</th>
+                                <th>address</th>
+                             </tr>
+                      </thead>
+                  
+                      <tbody class=" table-striped">
+                          <tr>
+                               <td>1</td>
+                               <td>doston</td>
+                               <td>sayfulloyev</td>
+                               <td>buxoro</td>
+                          </tr>
+                          <tr>
+                               <td>2</td>
+                               <td>sherzod</td>
+                               <td>sayfulloyev</td>
+                               <td>buxoro</td>
+                          </tr>
+                          <tr>
+                               <td>3</td>
+                               <td>azam</td>
+                               <td>qudratov</td>
+                               <td>buxoro</td>
+                          </tr>
+                          <tr>
+                               <td>4</td>
+                               <td>javohir</td>
+                               <td>sayfulloyev</td>
+                               <td>buxoro</td>
+                          </tr>
+                      </tbody> 
+
+                  </table>
+             </div>
+         </div>
+     </div>
+
+</body>
+</html>
